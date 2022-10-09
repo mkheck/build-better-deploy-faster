ddd1569331879be6ac3b7a8bbd6655915fd1bc22
Mark Heckler <mark.heckler@gmail.com>
2022-10-09 16:11:11 -0500

    Updated gateway-service

--- gateway-service/src/main/java/com/thehecklers/gatewayservice/GatewayServiceApplication.java
+++ gateway-service/src/main/java/com/thehecklers/gatewayservice/GatewayServiceApplication.java
@@ -9,7 +9,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.reactive.function.client.WebClient;
 
-@EnableDiscoveryClient
 @SpringBootApplication
 public class GatewayServiceApplication {
 
